0ead7f84edef45d541d772009ad663ad534271ad
Cristian Conache <cconache@gmail.com>
2017-05-16 00:10:10 +0300

    Refactored DAO classes and services

--- src/main/java/api/v1/Dao/RecipeDAO.java
+++ src/main/java/api/v1/Dao/RecipeDAO.java
@@ -1,14 +1,12 @@
 package api.v1.Dao;
 import api.v1.Models.Recipe;
-import net.minidev.json.JSONObject;
-
 import java.util.List;
 import java.util.Set;
 
 public interface RecipeDAO {
-    List<JSONObject> listRecipes(Integer initial_row, Integer rows);
-    JSONObject getRecipeByUuid(String uuid);
-    JSONObject getRecipePreview(String uuid);
-    List<JSONObject> searchRecipesByKeyword( String keyword, Integer initial_row, Integer rows );
+    List listRecipes(Integer initial_row, Integer rows);
+    Recipe getRecipeByUuid(String uuid);
+    Recipe getRecipePreview(String uuid);
+    List searchRecipesByKeyword( String keyword, Integer initial_row, Integer rows );
     Integer addRecipe(Recipe recipe, Set images, Set ingredients, Set steps);
 }
--- src/main/java/api/v1/Services/StepsServiceImpl.java
+++ src/main/java/api/v1/Services/StepsServiceImpl.java
@@ -2,30 +2,16 @@ package api.v1.Services;
 
 import api.v1.Dao.StepsDAO;
 import api.v1.Dao.StepsDAOImpl;
-import api.v1.Models.Step;
+import api.v1.Utils.DataFormatter;
 import net.minidev.json.JSONObject;
-import java.util.ArrayList;
 import java.util.List;
 
 public class StepsServiceImpl implements StepsService {
-    private StepsDAO stepsDAO = new StepsDAOImpl();
-
+    private final StepsDAO stepsDAO = new StepsDAOImpl();
+    private final DataFormatter formatter = new DataFormatter();
     @Override
     public List<JSONObject> getStepsByUuid(String uuid){
-        return jsonify( stepsDAO.getStepsByUuid(uuid) );
-    }
-
-    private JSONObject jsonify(Step s){
-        JSONObject res = new JSONObject();
-        res.appendField("task",s.getName());
-        res.appendField("description", s.getDescription());
-        return res;
-    }
-
-    private List<JSONObject> jsonify(List<Step> steps){
-        List<JSONObject> jsonSteps = new ArrayList<>();
-        if( steps != null ) for( Step step : steps) jsonSteps.add(jsonify(step));
-        return jsonSteps;
+        return formatter.jsonify( stepsDAO.getStepsByUuid(uuid) );
     }
 
 }
