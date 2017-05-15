9b879df6c6c3ea2992b8f7d019a60e35e6ffa73f
vladeugeniu <h5_em_1@yahoo.com>
2017-05-16 02:32:07 +0300

    test search

--- src/main/java/api/v1/Tests/RecipesControllerTest.java
+++ src/main/java/api/v1/Tests/RecipesControllerTest.java
@@ -1,20 +1,16 @@
 package api.v1.Tests;
 
+import api.v1.Controllers.RecipesController;
 import api.v1.Utils.JSONRecipeValidator;
-import nyla.solutions.global.json.JSON;
-import org.apache.http.protocol.HTTP;
-import org.junit.After;
+import net.minidev.json.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
-
-import api.v1.Controllers.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import static org.junit.Assert.*;
-import java.util.List;
+import java.util.*;
 
-import  net.minidev.json.JSONObject;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by Vlad Stefan on 5/14/2017.
@@ -44,16 +40,18 @@ public class RecipesControllerTest {
     }
 
     @Test
-    public void preview() throws Exception {
+    public void preview(){
 
         succesRequestPreview();
         failRequestPreview();
     }
 
     @Test
-    public void search() throws Exception {
-    }
+    public void search(){
 
+        succesRequestSearch();
+        failRequestSearch();
+    }
 
     /**
      * Tests index method of RecipesController class
@@ -76,12 +74,10 @@ public class RecipesControllerTest {
         JSONRecipeValidator recipeValidator = new JSONRecipeValidator();
         try {
             for (JSONObject recipe : recipesList) {
-
                 assertEquals(true, recipeValidator.isValid(recipe));
             }
         }
         catch (Exception e) {
-
             System.out.println("couldn't iterate through recipes in index method");
         }
     }
@@ -92,12 +88,13 @@ public class RecipesControllerTest {
      */
     private void failRequestIndex() {
 
-        ResponseEntity resultNegativeInt = recipesControllerTest.index(-1);
-        assertEquals(HttpStatus.BAD_REQUEST, resultNegativeInt.getStatusCode());
+        ResponseEntity result = recipesControllerTest.index(-1);
+        assertEquals(result.getStatusCode(), result.getStatusCode());
 
-        JSONObject body = (JSONObject) resultNegativeInt.getBody();
+        JSONObject body = (JSONObject) result.getBody();
         List<JSONObject> recipesList =  (List<JSONObject>) body.get("recipes");
         assertEquals(0, recipesList.size());
+
     }
 
     /**
@@ -116,15 +113,12 @@ public class RecipesControllerTest {
         JSONObject recipe= (JSONObject) result.getBody();
         JSONRecipeValidator validator = new JSONRecipeValidator();
         assertEquals(true,validator.isValidFull(recipe));
-
     }
-
 
     /**
      * Tests show method of RecipesController class
      * with a wrong parameter
      */
-
     private void failRequestShow(){
 
         String validUid = "-90a731f7-a60e-4a51-bdb8-8aa46764eace";
@@ -137,6 +131,10 @@ public class RecipesControllerTest {
         assertEquals(0, body.size());
     }
 
+    /**
+     * Tests preview method of RecipesController class
+     * with a valid parameter
+     */
     private void succesRequestPreview(){
 
         String validUid = "90a731f7-a60e-4a51-bdb8-8aa46764eace";
@@ -148,9 +146,12 @@ public class RecipesControllerTest {
         JSONObject recipe = (JSONObject) result.getBody();
         JSONRecipeValidator validator = new JSONRecipeValidator();
         assertEquals(true,validator.isValid(recipe));
-
     }
 
+    /**
+     * Tests preview method of RecipesController class
+     * with a wrong parameter
+     */
     private void failRequestPreview(){
 
         String validUid = "-90a731f7-a60e-4a51-bdb8-8aa46764eace";
@@ -161,6 +162,117 @@ public class RecipesControllerTest {
 
         JSONObject body = (JSONObject)result.getBody();
         assertEquals(0, body.size());
+    }
+
+    /**
+     * Tests search method of RecipesController class
+     * with a valid parameter
+     */
+    private void succesRequestSearch(){
+
+        HashMap<String,Object> request = new HashMap<String,Object>();
+        request.put("keywords", "grill");
+        request.put("page", new Integer(1));
+        int page = 1;
+        while(true) {
+
+            ResponseEntity result = recipesControllerTest.search(request);
+
+            if(result.getStatusCode().equals(HttpStatus.NOT_FOUND))
+                break;
+
+            succesResponse(result);
+
+            JSONObject body = (JSONObject) result.getBody();
+            Set<JSONObject> recipesList = (Set<JSONObject>) body.get("recipes");
+            assert (recipesList.size() > 0);
+
+            JSONRecipeValidator recipeValidator = new JSONRecipeValidator();
+            try {
+                for (JSONObject recipe : recipesList) {
+                    assert (recipe.getAsString("name").toLowerCase().contains("grill"));
+                }
+            } catch (Exception e) {
+                System.out.println("couldn't iterate through recipes in index method");
+            }
+        page ++;
+            request.put("page", page);
+
+        }
+    }
+
+    /**
+     * Tests search method of RecipesController class
+     * with a wrong parameter
+     */
+    private void failRequestSearch(){
+
+        HashMap<String,Object> request1 = new HashMap<String,Object>();
+        request1.put("page1",new Integer(1));
+        request1.put("keywords","vegetable grill");
+        ResponseEntity result = recipesControllerTest.search(request1);
+        failResponse(result, HttpStatus.BAD_REQUEST);
+
+        request1.put("something", new Double(5));
+        result = recipesControllerTest.search(request1);
+        failResponse(result, HttpStatus.BAD_REQUEST);
+
+        HashMap<String,Object> request2 = new HashMap<String,Object>();
+        request2.put("page",new Integer(1));
+        request2.put("keyworrds","grill");
+        result = recipesControllerTest.search(request2);
+        failResponse(result, HttpStatus.BAD_REQUEST);
+
+        HashMap<String,Object> request3 = new HashMap<String,Object>();
+        request3.put("page",new Integer(-1));
+        request3.put("keywords","grill");
+        result = recipesControllerTest.search(request3);
+        failResponse(result, HttpStatus.BAD_REQUEST);
+
+        HashMap<String,Object> request4 = new HashMap<String,Object>();
+        request4.put("page",new Integer(1000000));
+        request4.put("keywords","grill");
+        result = recipesControllerTest.search(request4);
+        failResponse(result, HttpStatus.NOT_FOUND);
+
+        HashMap<String,Object> request5 = new HashMap<String,Object>();
+        request5.put("page",new Integer(1));
+        request5.put("keywords","I just wanna go home, :'( *sad face*");
+        result = recipesControllerTest.search(request5);
+        failResponse(result, HttpStatus.NOT_FOUND);
+
 
     }
+
+    private void succesResponse( ResponseEntity result){
+
+        assertEquals(HttpStatus.OK, result.getStatusCode());
+        assertEquals(true,result.hasBody());
+
+        JSONObject body = (JSONObject) result.getBody();
+        assertEquals(true,body.containsKey("recipes"));
+
+        Set<JSONObject> recipesList =  (Set<JSONObject>) body.get("recipes");
+        assert(recipesList.size() > 0);
+
+        JSONRecipeValidator recipeValidator = new JSONRecipeValidator();
+        try {
+            for (JSONObject recipe : recipesList) {
+                assertEquals(true, recipeValidator.isValid(recipe));
+            }
+        }
+        catch (Exception e) {
+            System.out.println("couldn't iterate through recipes in index method");
+        }
+    }
+
+    private void failResponse(ResponseEntity result, HttpStatus status){
+
+        assertEquals(status, result.getStatusCode());
+
+        JSONObject body = (JSONObject) result.getBody();
+        Set<JSONObject> recipesList =  (Set<JSONObject>) body.get("recipes");
+        assertEquals(0, recipesList.size());
+    }
+
 }
