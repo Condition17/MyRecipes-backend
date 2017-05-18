891fd7255cd0983e3f3b80374cae87f932ba0aa1
Cristian Conache <cconache@gmail.com>
2017-05-18 13:08:43 +0300

    Test fail update for travis investigation

--- src/test/java/api/v1/Controllers/StepsControllerTest.java
+++ src/test/java/api/v1/Controllers/StepsControllerTest.java
@@ -44,7 +44,7 @@ public class StepsControllerTest {
 
     private void succesRequestShow(){
 
-        String validUid = "291d2de5-9f8f-420f-8402-8aa444792214";
+        String validUid = "291d2de5-9f8f-420f-8402-8aa444792";
 
         ResponseEntity result = stepsControllerTest.show(validUid);
         assertEquals(HttpStatus.OK, result.getStatusCode());
